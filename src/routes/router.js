1879d58b2c911f85ef36a0817bbf24adb18345db
reshad21 <miakala7@gmail.com>
2023-04-19 11:28:57 +0600

    basic router setup complete

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import Dashbord from '../Layout/Dashbord/Dashbord';
+import Main from '../Layout/Main/Main';
+import Home from '../pages/Main/Home';
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+        ],
     },
+    {
+        path: "/dashboard",
+        element: <Dashbord></Dashbord>,
+        children: [
+        //   {
+        //     path: "/dashboard",
+        //     element: <ProductList />,
+        //   },
+        //   {
+        //     path: "add-product",
+        //     element: <AddProduct />,
+        //   },
+        ],
+      },
 ]);
 
 export default router;
